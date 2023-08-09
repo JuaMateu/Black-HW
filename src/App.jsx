1d75a39585d72b1be64b2d4cc74c0a708c146b98
Juan Mateu <juanma.mateu@gmail.com>
2023-08-09 00:45:03 -0300

    set cartContext

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,25 @@ import Home from "./Components/Home";
 import ItemListConteiner from "./Components/ItemListConteiner/ItemListConteiner";
 import ItemDetailConteiner from "./Components/ItemDetailConteiner/ItemDetailCointeiner";
 
-
+import ShoppingCartProvider from './context/cartContext'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        {/* <ItemListConteiner greeting={"Bienvenidos a Black Hardware"} /> */}
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/shop' element={<ItemListConteiner />} />
-          <Route exact path='/categories/:category' element={<ItemListConteiner />} />
-          <Route exact path='/item/:id' element={<ItemDetailConteiner />} />
-          <Route exact path='/aboutus' element={<Home />} />
-          <Route exact path='/contact' element={<Home />} />
-        </Routes>
+        <ShoppingCartProvider>
+          <NavBar />
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/shop' element={<ItemListConteiner />} />
+            <Route exact path='/categories/:category' element={<ItemListConteiner />} />
+            <Route exact path='/item/:id' element={<ItemDetailConteiner />} />
+            <Route exact path='/aboutus' element={<Home />} />
+            <Route exact path='/contact' element={<Home />} />
+            <Route  path='*' element={<h4> error 404 </h4>} />
+          </Routes>
+        </ShoppingCartProvider>
       </BrowserRouter>
       <Footer />
     </>
--- src/Components/NavBar/CartWidget.jsx
+++ src/Components/NavBar/CartWidget.jsx
@@ -1,11 +1,16 @@
 import { FaShoppingCart } from "react-icons/fa";
+import { useContext } from 'react';
+import { CartContext } from '../../context/cartContext';
 
-const CartWidget = ({number}) => {
+const CartWidget = () => {
+
+  const {cartCant } = useContext(CartContext)
+
   return (
     <div className="relative bg-secondary p-2 rounded-full cursor-pointer z-0">
       <FaShoppingCart size={35} color="#D7DDE6" className=" p-1 z-10" />
-       <span className="text-primary absolute top-8 left-9 bg-terciary rounded-full px-1 text-xs font-extrabold z-20">{ number }</span>
-     </div>
+      <span className="text-primary absolute top-8 left-9 bg-terciary rounded-full px-1 text-xs font-extrabold z-20">{cartCant}</span>
+    </div>
   )
 }
 
